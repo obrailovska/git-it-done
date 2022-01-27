f6473b0b0374408c6621dd9e1db969d6d805a45e
Olha Brailovska <obrailovska@Olhas-Air.home>
2022-01-26 23:03:16 -0500

    polished styles

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,19 +2,23 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-var searchTerm;
+
 var formSubmitHandler = function (event) {
+  // prevent page from refreshing
   event.preventDefault();
+
   // get value from input element
   var username = nameInputEl.value.trim();
 
   if (username) {
     getUserRepos(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
   }
-  console.log(event);
 };
 
 var getUserRepos = function (user) {
@@ -23,28 +27,32 @@ var getUserRepos = function (user) {
 
   // make a get request to url
   fetch(apiUrl)
-  .then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        displayRepos(data, user);
-      });
-    } else {
-      alert('Error: GitHub User Not Found');
-    }
-  })
-  .catch(function(error) {
-    // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to GitHub");
-  });
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to GitHub");
+    });
+};
 
-getUserRepos("");
 var displayRepos = function (repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
+
+  repoSearchTerm.textContent = searchTerm;
+
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
     // format repo name
@@ -57,6 +65,10 @@ var displayRepos = function (repos, searchTerm) {
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
     // create a status element
     var statusEl = document.createElement("span");
     statusEl.classList = "flex-row align-center";
@@ -75,14 +87,10 @@ var displayRepos = function (repos, searchTerm) {
     // append to container
     repoEl.appendChild(statusEl);
 
-    // append to container
-    repoEl.appendChild(titleEl);
-
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
 };
-// clear old content
-repoContainerEl.textContent = "";
-repoSearchTerm.textContent = searchTerm;
+
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
